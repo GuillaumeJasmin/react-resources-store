7e4026bd8a878d29023f0c141e09a7b03a9d44f8
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-11-19 11:19:35 +0100

    chore: export new types

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { Provider } from './context';
 export { useRequest } from './useRequest';
 export { useLazyRequest } from './useLazyRequest';
 export { createReducers } from './createReducers';
-export { Schema } from './types';
+export { Schema, StoreState, Resolver } from './types';
 export { getRequest } from './getRequest';
 export { createAxiosResolver } from './resolvers/axios';
 export { createFetchResolver } from './resolvers/fetch';

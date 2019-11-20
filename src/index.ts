b8c0ccad5c303d7a4c37bf46cb9901ff0dddab08
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-11-20 16:31:27 +0100

    chore: export Context

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { Provider } from './context';
+export { Provider, Context } from './context';
 export { useRequest } from './useRequest';
 export { useLazyRequest } from './useLazyRequest';
 export { createReducers } from './createReducers';
